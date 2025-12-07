65284a7194113660c39a4d17952cfd93429a92e1
usarovakamilla <usarovakamilla13@gmail.com>
2025-12-07 14:28:56 +0500

    Update header.php

--- header.php
+++ header.php
@@ -1,13 +1,12 @@
-
 <header>
     <div class="logo">
         <a href="#"><img src="logo.png" alt="Logo"></a>
     </div>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Главная</a></li>
             <li><a href="about.php">0 нас</a></li>
-            <li><a href="contacts.php">Contacts</a></li>
+            <li><a href="contacts.php">Контакты</a></li>
         </ul>
     </nav>
 </header>
