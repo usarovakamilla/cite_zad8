e29c43e43b6ff2defcf93720f018a6e374c135a5
usarovakamilla <usarovakamilla13@gmail.com>
2025-12-07 14:23:27 +0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+
+<header>
+    <div class="logo">
+        <a href="#"><img src="logo.png" alt="Logo"></a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">0 нас</a></li>
+            <li><a href="contacts.php">Contacts</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Заголовок сайта</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+    include 'header.php';
+    include 'main.php';
+    include 'footer.php';
+?>
+</body>
+</html>
